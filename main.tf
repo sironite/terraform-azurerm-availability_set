2bbe4e3e2d4086c65ecca11529cd3bae924ebf9d
WDamman <58424695+TheIronRock95@users.noreply.github.com>
2023-06-28 14:33:35 +0200

    feat: firstrelease

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 resource "azurerm_availability_set" "example" {
-  name                         = var.availability_set_name
-  location                     = var.location
-  managed                      = var.managed
+  name     = var.availability_set_name
+  location = var.location
+  managed  = var.managed
+
   platform_fault_domain_count  = var.platform_fault_domain_count
   platform_update_domain_count = var.platform_update_domain_count
   proximity_placement_group_id = var.proximity_placement_group_id
